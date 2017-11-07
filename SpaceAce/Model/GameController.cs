6688f43c1be7b30e6ad3441c38ba6d6421ea11b0
JoannaMadNat <jalma146@students.bju.edu>
2017-11-07 17:13:27 -0500

    Put in save/load method comments

--- SpaceAce/Model/GameController.cs
+++ SpaceAce/Model/GameController.cs
@@ -33,11 +33,20 @@ namespace Model
 
         public void Save()
         {
-            // TODO: save game state to GameData and save that to disk
+            //TODO: run for loop and collect all strings generated from each object in world
+            //format of string: TypeOfEntity,instance variable1, instance variable2, instance variable3, instance variable4 ... so on
+            //                  TypeOfEntity,instance variable1, instance variable2, instance variable3, instance variable4 ... so on
+
+            //TODO: Write each line to file, leave GameController class at end             
         }
-        public void Load()
+        public GameController Load()
         {
-            // TODO: load a state form GameData
+            //TODO: make check if file exists, if no file exists, create world with default settings
+
+            //TODO: read from file and create entities with the specifications found in the file
+
+            //Returns new GameController class with all the specifications in the file
+            return new GameController();
         }
 
         public void updatePlayer(PlayerAction action)
